7fcf4e09e707bfcaea2fb5bc3f3ac36a1976bbbc
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-02-14 20:49:27 +0000

    Update settings.py
    Changed cors settings.

--- backend/settings.py
+++ backend/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'https://teaminnovation.netlify.app'
 ]
