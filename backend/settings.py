71ea0f8e2213ebfc462404843ad12aa6bc79e25c
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-02-14 21:31:59 +0000

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -127,9 +127,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    'https://teaminnovation.netlify.app'
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 django_heroku.settings(locals())
